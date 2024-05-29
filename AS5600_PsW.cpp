8220f881a87523b2c0fba991cc41842446bdc7ef
Neo <salehi.yasin@gmail.com>
2024-05-29 21:05:37 +0330

    priliminary code

--- AS5600_PsW.cpp
+++ AS5600_PsW.cpp
@@ -1,1 +1,14 @@
 #include "AS5600_PsW.h"
+
+AS5600_PsW::init() 
+{
+    Wire.beginTransmission(sensorI2CAddress); // I2C address of AS5600
+    Wire.write(ConfigReg); // Address of CONF register (0x07)
+    Wire.write(0b00000000); // MSB of CONF register (default value)
+    Wire.write(0b00000001); // LSB of CONF register 
+}
+
+AS5600_PsW::rawAngle()
+{
+
+}
--- AS5600_PsW.h
+++ AS5600_PsW.h
@@ -1,14 +1,30 @@
 #ifndef _AS5600_PsW_cbbca5380e886eb45660f9565d9e4d78f484a2d7d60682f313ec351bef31dbf4
 #define _AS5600_PsW_cbbca5380e886eb45660f9565d9e4d78f484a2d7d60682f313ec351bef31dbf4
 
+#include <Arduino.h>
+#include "Wire.h"
+
+class AS5600_PsW
+	{
+	public:
+		void init();
+		int rawAngle();
 
 
 
 
+	private:
+		//Masks
+		static const uint8_t Mask01 = 0b00000011;
+		static const uint8_t Mask23 = 0b00001100;
+		static const uint8_t Mask45 = 0b00110000;
+		static const uint8_t Mask67 = 0b11000000;
 
+		//Registers
+		static const uint8_t sensorI2CAddress = 0x36; //Sensor Address
+		static const uint8_t ConfigReg = 0x07; //Config Register
+		static const uint8_t rawAngle = 0x0c; // raw angle
+		static const uint8_t Angle = 0x0e; // scaled output value of raw angle
 
-
-
-
-
+};
 #endif
