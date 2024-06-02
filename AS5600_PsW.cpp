750e7103f37474b36155f17c4ab756899b73eae7
Neo <salehi.yasin@gmail.com>
2024-06-02 09:26:03 +0330

    Degree angle added and read changed

--- AS5600_PsW.cpp
+++ AS5600_PsW.cpp
@@ -11,26 +11,15 @@
 void AS5600_PsW::init()
 {	
 	Wire.begin();
-	
-    Wire.beginTransmission(sensorI2CAddress); // I2C address of AS5600
-    Wire.write(configRegister); // Address of CONF register (0x07)
-    Wire.write(0b00000000); // MSB of CONF register (default value)
-    Wire.write(0b00000001); // LSB of CONF register 
-	wireCondition = Wire.endTransmission();
-	
-	chechWireCondition(wireCondition);
-	
 }
 
-
-int AS5600_PsW::rawAngle()
+word AS5600_PsW::readWire(uint8_t _wireRegisterToRead)
 {
-	uint16_t rawAngle{ 0 };
 	uint8_t MSB{ 0 };
 	uint8_t LSB{ 0 };
 
 	Wire.beginTransmission(sensorI2CAddress);
-	Wire.write(rawAngleRegister); //invove the sensor
+	Wire.write(_wireRegisterToRead); //invove the sensor
 	wireCondition = Wire.endTransmission();
 
 	if (chechWireCondition(wireCondition) == 0) return;
@@ -45,8 +34,7 @@ int AS5600_PsW::rawAngle()
 	return (MSB << numberOfLsbBits) | LSB;
 }
 
-
-bool AS5600_PsW::chechWireCondition(int _wireCondition) 
+bool AS5600_PsW::chechWireCondition(int _wireCondition)
 {
 	if (_wireCondition != 0) {
 		//print sth here
@@ -62,8 +50,29 @@ bool AS5600_PsW::chechWireCondition(int _wireCondition)
 		wireCondition = -1;
 		return 0;
 	}
-	else{ 
+	else {
 		wireCondition = -1;
 		return 1;
 	}
 }
+
+int AS5600_PsW::rawAngle()
+{
+	return readWire(rawAngleRegister);
+}
+
+float degreeAngle()
+{
+	return readWire(degreeAngleRegister);
+}
+
+
+
+
+//Wire.beginTransmission(sensorI2CAddress); // I2C address of AS5600
+//Wire.write(configRegister); // Address of CONF register (0x07)
+//Wire.write(0b00000000); // MSB of CONF register (default value)
+//Wire.write(0b00000001); // LSB of CONF register 
+//wireCondition = Wire.endTransmission();
+//
+//chechWireCondition(wireCondition);
